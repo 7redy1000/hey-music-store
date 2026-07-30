adafd5a35c3e29b871805944685956a76aacc687
7redy1000 <fredy.software7@gmail.com>
2026-07-30 19:10:44 -0400

    Add function to notify product addition to cart

---
+++ script.js
@@ -0,0 +1,9 @@
+function agregarAlCarrito(nombreProducto) {
+    const notificacion = document.getElementById('notificacion');
+    notificacion.textContent = `🛒 ¡${nombreProducto} agregado al carrito!`;
+    notificacion.classList.remove('oculta');
+
+    setTimeout(() => {
+        notificacion.classList.add('oculta');
+    }, 3000);
+}
